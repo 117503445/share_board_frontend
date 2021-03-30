db3ff3f3c241cab3e3484b91c64e1de87ff49cb9
117503445 <t117503445@gmail.com>
2021-03-30 11:56:09 +0800

    feat: support websocket

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -18,6 +18,66 @@
 
 <script>
 let canvas;
+var ws;
+
+function connectWS() {
+  let HOST_WS = "wss://shareboard.goldhome.117503445.top:18888";
+  let HOST_HTTP = "https://shareboard.goldhome.117503445.top:18888";
+
+  // HOST_WS = "ws://localhost";
+  // HOST_HTTP = "http://localhost";
+
+  ws = new WebSocket(HOST_WS + "/api/ws");
+  ws.onmessage = function (msg) {
+    console.log("ws onmessage");
+    console.log(msg.data.length);
+    console.log(msg.data);
+    let receivedJson = JSON.parse(msg.data);
+
+    let updateType = receivedJson["type"];
+    let receivedData = receivedJson["data"];
+
+    if (updateType == "replace") {
+      console.log("replace");
+      canvas.loadFromJSON(JSON.stringify(receivedData));
+    } else if (updateType == "add") {
+      console.log("add");
+      let oldCanvasJson = canvas.toJSON();
+      oldCanvasJson["objects"].push(receivedData);
+      canvas.loadFromJSON(JSON.stringify(oldCanvasJson));
+    } else if (updateType == "status") {
+      console.log("status");
+      canvas.loadFromJSON(JSON.stringify(receivedData));
+    } else {
+      console.log(updateType + " ???");
+    }
+  };
+
+  ws.onopen = function (event) {
+    console.log("WebSocket is ready now.");
+
+    // const Http = new XMLHttpRequest();
+    // Http.open("GET", HOST_HTTP + "/api/v1/board/1");
+    // Http.send();
+
+    // Http.onreadystatechange = (e) => {
+    //     let js = Http.responseText;
+    //     //console.log(js);
+    //     canvas.loadFromJSON(js);
+    // }
+    var json = JSON.stringify({
+      type: "status",
+      data: { boardid: "1", pagenumber: 1 },
+    });
+    ws.send(json);
+  };
+
+  ws.onclose = function (event) {
+    console.log("WebSocket is closed now.");
+    connectWS();
+  };
+}
+connectWS();
 
 export default {
   mounted() {
@@ -43,6 +103,20 @@ export default {
       }
       canvas.discardActiveObject(); //清除选中框
     });
+    function canvas_up(options) {
+      if (this.drawMode === "eraser") {
+        var json = JSON.stringify({ type: "replace", data: canvas.toJSON() });
+        ws.send(json);
+      } else {
+        let objects = canvas.toJSON()["objects"];
+        let lastObject = objects[objects.length - 1];
+        var json = JSON.stringify({ type: "add", data: lastObject });
+        console.log(json)
+        ws.send(json);
+      }
+    }
+    canvas.on("mouse:up", canvas_up);
+    canvas.on("touch:up", canvas_up);
 
     // https://www.npmjs.com/package/@arch-inc/fabricjs-psbrush 压感笔刷
     let brush = new fabric.PSBrush(canvas);
